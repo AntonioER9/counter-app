46eb9ce51bef8579e6838f29e37bba6aa3460921
AntonioER9 <aespinozareyes9@gmail.com>
2024-07-03 12:09:39 -0400

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CounterApp } from './CounterApp';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CounterApp value={10} />
   </React.StrictMode>
 );
 
---
+++ src/tests/demo.test.ts
@@ -0,0 +1,15 @@
+describe('Test in DemoTest', () => { 
+
+  test('This test must not fail', () => { 
+
+    // 1. Arrange
+    const message1 = 'Hello World';
+  
+    // 2. Act
+    const message2 = message1.trim();
+  
+    // 3. Assert
+    expect(message1).toBe(message2);
+  });
+
+});
